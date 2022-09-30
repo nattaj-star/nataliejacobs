f9f0e5baee5e05dad04c2f15aa9d488412375155
Glitch (glitch-hello-website) <none>
2022-09-30 03:21:20 +0000

    😼👾 Checkpoint ./sketch.js:13523063/543 ./index.html:13523063/3736 ./style.css:13523063/4157

---
+++ sketch.js
@@ -0,0 +1,34 @@
+var c1, c2, c3;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  c1 = color(242, 150, 51);
+  c2 = color(237, 82, 31);
+  
+  setGradient(c1, c2);
+}
+
+function draw() {
+  stroke(9, 93, 189,50);
+ ellipse(mouseX, mouseY, 40, 40);
+  
+}
+
+
+
+
+
+function setGradient(c1, c2) {
+  // noprotect
+  noFill();
+  for (var y = 0; y < height; y++) {
+    var inter = map(y, 0, height, 0, 1);
+    var c = lerpColor(c1, c2, inter);
+    stroke(c);
+    line(0, y, width, y);
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
